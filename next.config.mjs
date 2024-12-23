3a8fd860d94140c7f5d348202964b2505e1fbb20
Nils Wittler <nilswe@posteo.de>
2024-12-23 17:36:01 +0100

    add base for redis cache handler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+import path from 'node:path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  cacheMaxMemorySize: 0,
+  cacheHandler: process.env.NODE_ENV === 'production' ? path.resolve('./cache-handler.mjs') : undefined,
+  // experimental: {
+  //   // This is required for the experimental feature of pre-populating the cache with the initial data
+  //   instrumentationHook: true,
+  // },
 };
 
 export default nextConfig;
