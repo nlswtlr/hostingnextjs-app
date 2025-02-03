3a022587f29112d267d54c7c5960a8500190eb4b
Nils Wittler <nilswe@posteo.de>
2025-02-03 16:55:02 +0100

    update redis integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-import path from 'node:path';
+import path from "node:path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  cacheMaxMemorySize: 0,
-  cacheHandler: process.env.NODE_ENV === 'production' ? path.resolve('./cache-handler.mjs') : undefined,
+  cacheMaxMemorySize: process.env.REDIS_URL ? 0 : undefined,
+  cacheHandler: process.env.REDIS_URL ? path.resolve("./cache-handler.mjs") : undefined,
   // experimental: {
   //   // This is required for the experimental feature of pre-populating the cache with the initial data
   //   instrumentationHook: true,
